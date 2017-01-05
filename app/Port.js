19e99c2cedf08aa3ad672f34879b7787dfdeea61
Johan Li <johan@johanli.com>
2017-01-05 20:14:34 +0100

    decided to ditch physics system. Implemented new collision logic

--- app/Port.js
+++ app/Port.js
@@ -1,7 +1,7 @@
 class Port extends Phaser.State {
 
     preload() {
-        this.load.spritesheet('joao', '/img/joao.png', 24, 32);
+        this.load.spritesheet('joao', '/img/joao.png', 32, 32);
         this.load.spritesheet('npcs', '/img/npcs.png', 32, 32);
         this.load.tilemap('lisbon', '/tilemaps/lisbon.json', null, Phaser.Tilemap.TILED_JSON);
         this.load.image('tileset1.2', '/img/tileset1.2.png');
@@ -9,11 +9,9 @@ class Port extends Phaser.State {
     }
 
     create() {
-        this.physics.startSystem(Phaser.Physics.ARCADE);
         this.addMap();
         this.addNpc();
         this.addPlayer();
-
         this.addMusic();
 
         this.cursors = this.input.keyboard.createCursorKeys();
@@ -33,41 +31,32 @@ class Port extends Phaser.State {
         this.map.setLayer(this.layer);
 
         this.map.setCollisionBetween(29, 240);
-        this.setLeftRightTileCollisions();
 
         this.layer.debug = true;
     }
 
     addNpc() {
         this.npc = this.add.group();
-        this.npc.create(689, 960, 'npcs', 0);
-        this.npc.create(609, 752, 'npcs', 2);
-        this.npc.create(897, 560, 'npcs', 4);
-        this.physics.enable(this.npc);
+        this.npc.create(688, 976, 'npcs', 0);
+        this.npc.create(608, 768, 'npcs', 2);
+        this.npc.create(896, 576, 'npcs', 4);
 
         this.npc.forEach((sprite) => {
-            sprite.body.immovable = true;
+            sprite.anchor.setTo(0, 0.5);
             sprite.animations.add('flash', [sprite.frame, sprite.frame + 1]);
             sprite.animations.play('flash', 5, true);
         });
     }
 
     addPlayer() {
-        this.player = this.add.sprite(868, 1088, 'joao', 0);
+        this.player = this.add.sprite(864, 1104, 'joao', 0);
+        this.player.anchor.setTo(0, 0.5);
         this.camera.follow(this.player);
 
-        this.physics.enable(this.player);
-        this.physics.arcade.gravity.y = 0;
-        this.player.body.collideWorldBounds = true;
-
-        this.player.animations.add('left', [2,3], 10);
-        this.player.animations.add('right', [4,5], 10);
-        this.player.animations.add('up', [6,7], 10);
-        this.player.animations.add('down', [0,1], 10);
-
-        this.player.body.setSize(24, 16, 0, 16);
-
-        this.speed = 500;
+        this.player.animations.add('left', [2,3], 5);
+        this.player.animations.add('right', [4,5], 5);
+        this.player.animations.add('up', [6,7], 5);
+        this.player.animations.add('down', [0,1], 5);
     }
 
     addMusic() {
@@ -77,55 +66,73 @@ class Port extends Phaser.State {
     }
 
     movePlayer() {
-        this.physics.arcade.collide(this.player, this.layer);
-        this.physics.arcade.collide(this.player, this.npc);
-        this.player.body.velocity.y = 0;
-        this.player.body.velocity.x = 0;
+        this.setDestination();
+
+        if (this.noDestinationCollision()) {
+            this.updatePlayerLocation();
+        }
+    }
+
+    setDestination() {
+        let tileSize = 16;
+
+        this.destination = {
+            x: this.player.x,
+            y: this.player.y,
+        };
 
         if (this.cursors.up.isDown) {
-            this.player.body.velocity.y = -this.speed;
-            this.player.play('up');
+            this.destination.y -= tileSize;
         } else if (this.cursors.down.isDown) {
-            this.player.body.velocity.y = this.speed;
-            this.player.play('down');
+            this.destination.y += tileSize;
         }
         else if (this.cursors.left.isDown) {
-            this.player.body.velocity.x = -this.speed;
-            this.player.play('left');
+            this.destination.x -= tileSize;
         }
         else if (this.cursors.right.isDown) {
-            this.player.body.velocity.x = this.speed;
-            this.player.play('right');
-        } else {
-            this.player.animations.stop();
+            this.destination.x += tileSize;
         }
+
+        this.destination.tileX = this.destination.x / tileSize;
+        this.destination.tileY = this.destination.y / tileSize;
+    }
+
+    noDestinationCollision() {
+        let noCollision = true;
+        let destinationTile = this.map.getTile(this.destination.tileX, this.destination.tileY);
+        let destinationTileRight = this.map.getTile(this.destination.tileX + 1, this.destination.tileY);
+        let leftmostCollisionIndices = [31, 34];
+        let rightmostCollisionIndices = [39, 40];
+
+        if (!destinationTile || !destinationTileRight)
+            return false;
+
+        if (destinationTile.collides || destinationTileRight.collides)
+            noCollision = false;
+
+        if (leftmostCollisionIndices.includes(destinationTile.index)) {
+            noCollision = true;
+        }
+
+        if (rightmostCollisionIndices.includes(destinationTileRight.index)) {
+            noCollision = true;
+        }
+
+        return noCollision;
+    }
+
+    updatePlayerLocation() {
+        this.player.x = this.destination.x;
+        this.player.y = this.destination.y;
     }
 
     render() {
-        this.game.debug.body(this.player);
+        this.game.debug.spriteInfo(this.player, 32, 32, '#ffffff');
+        this.game.debug.spriteBounds(this.player, '#ce0020', false);
 
         this.npc.forEach((sprite) => {
-            this.game.debug.body(sprite);
+            this.game.debug.spriteBounds(sprite, '#ce7524', false);
         });
-    }
-
-
-    // a few collision tiles in the game behave in such a way that the collision don't occur
-    // over their entire area. Instead, the collision only happens on either the leftmost or rightmost pixels.
-    setLeftRightTileCollisions() {
-        const leftCollisionIndices = [31, 34];
-        const rightCollisionIndices = [39];
-
-        for (let row of this.layer.map.layers[this.layer.index].data) {
-            for (let rowTile of row) {
-                if (leftCollisionIndices.includes(rowTile.index)) {
-                    rowTile.width = 4;
-                } else if (rightCollisionIndices.includes(rowTile.index)) {
-                    rowTile.width = 4;
-                    rowTile.worldX += 12;
-                }
-            }
-        }
     }
 }
 
