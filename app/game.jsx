237b2a122ebbdf6a06e0b734f6a6a5df860ccbfb
Johan Li <johan@johanli.com>
2017-11-15 20:13:51 +0100

    add loading spinner and prevent initial reflow

--- app/game.jsx
+++ app/game.jsx
@@ -4,12 +4,10 @@ import ReactDOM from 'react-dom';
 import { Left, Right, Building } from './interface.jsx';
 
 ReactDOM.render(
-  <div>
-    <div id="center">
-      <main id="app">
-        <canvas id="camera" width="1280" height="800" />
-      </main>
-    </div>
+  <div id="center">
+    <main id="app">
+      <canvas id="camera" width="1280" height="800" />
+    </main>
   </div>,
   document.getElementById('root')
 );
