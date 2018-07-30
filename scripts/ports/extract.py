b5db79342afcdccc90d4c0076a7d320ba2b361d5
Johan Li <johan@johanli.com>
2018-07-31 01:22:42 +0200

    add script for extracting port coordinates

---
+++ scripts/ports/extract.py
@@ -0,0 +1,38 @@
+import json
+import pathlib
+
+pathlib.Path('./output').mkdir(parents=True, exist_ok=True)
+
+
+def shift_world_map(x):
+    if x >= 1440:
+        return x - 1440
+
+    return x + 720
+
+
+ports = []
+
+with open('./raw/DATA1.015', 'rb') as file:
+    raw_bytes = file.read()
+
+byte_cursor = 20286
+
+for i in range(130):
+    x = (raw_bytes[byte_cursor + 3] << 8) | raw_bytes[byte_cursor + 2]
+    y = (raw_bytes[byte_cursor + 5] << 8) | raw_bytes[byte_cursor + 4]
+    name = raw_bytes[byte_cursor + 6:byte_cursor + 20].decode('utf-8').strip('\u0000')
+
+    x = shift_world_map(x)
+
+    ports.append({
+        'id': i,
+        'name': name,
+        'x': x,
+        'y': y,
+    })
+
+    byte_cursor += 20
+
+with open('./output/ports.json', 'w') as file:
+    json.dump(ports, file, indent=2)
