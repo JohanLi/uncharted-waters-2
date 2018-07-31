2fd8fc80b96101b0d11c577fb8c7548dc262ff29
Johan Li <johan@johanli.com>
2018-07-31 20:31:37 +0200

    add code for combining port tilemaps

--- scripts/ports/extract.py
+++ scripts/ports/extract.py
@@ -11,28 +11,44 @@ def shift_world_map(x):
     return x + 720
 
 
-ports = []
+def metadata_to_json():
+    ports = []
 
-with open('./raw/DATA1.015', 'rb') as file:
-    raw_bytes = file.read()
+    with open('./raw/DATA1.015', 'rb') as file:
+        raw_bytes = file.read()
 
-byte_cursor = 20286
+    byte_cursor = 20286
 
-for i in range(130):
-    x = (raw_bytes[byte_cursor + 3] << 8) | raw_bytes[byte_cursor + 2]
-    y = (raw_bytes[byte_cursor + 5] << 8) | raw_bytes[byte_cursor + 4]
-    name = raw_bytes[byte_cursor + 6:byte_cursor + 20].decode('utf-8').strip('\u0000')
+    for i in range(130):
+        x = (raw_bytes[byte_cursor + 3] << 8) | raw_bytes[byte_cursor + 2]
+        y = (raw_bytes[byte_cursor + 5] << 8) | raw_bytes[byte_cursor + 4]
+        name = raw_bytes[byte_cursor + 6:byte_cursor + 20].decode('utf-8').strip('\u0000')
 
-    x = shift_world_map(x)
+        x = shift_world_map(x)
 
-    ports.append({
-        'id': i,
-        'name': name,
-        'x': x,
-        'y': y,
-    })
+        ports.append({
+            'id': i,
+            'name': name,
+            'x': x,
+            'y': y,
+        })
 
-    byte_cursor += 20
+        byte_cursor += 20
 
-with open('./output/ports.json', 'w') as file:
-    json.dump(ports, file, indent=2)
+    with open('./output/ports.json', 'w') as file:
+        json.dump(ports, file, indent=2)
+
+
+def combine_tilemaps():
+    combined_tilemaps = b''
+
+    for i in range(101):
+        with open('./raw/PORTMAP.' + f'{i:03}', 'rb') as file:
+            combined_tilemaps += file.read()
+
+    with open('./output/port-tilemaps.bin', 'wb') as file:
+        file.write(combined_tilemaps)
+
+
+metadata_to_json()
+combine_tilemaps()
