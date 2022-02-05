966bb5530cfe66d5cd94e210ed33ba1b6abffa3a
Johan Li <hi@johan.li>
2022-02-05 13:42:46 +0100

    feat: listen to click and keydown to trigger sound autoplay. Toggling sound should not restart track

--- src/interface/sound/Sound.tsx
+++ src/interface/sound/Sound.tsx
@@ -2,6 +2,7 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 
 import React, { useEffect, useRef, useState } from 'react';
+import { VolumeOffIcon, VolumeUpIcon } from '@heroicons/react/solid';
 
 import { getRegionOrIfSupplyPort } from '../../port/portUtils';
 
@@ -15,12 +16,22 @@ import theMahout from './assets/the-mahout.ogg';
 import landOfLuxury from './assets/land-of-luxury.ogg';
 import moslemDance from './assets/moslem-dance.ogg';
 
-interface Props {
-  portId: string | null;
-  buildingId: string | null;
-}
+/*
+  https://developer.mozilla.org/en-US/docs/Web/Media/Autoplay_guide
 
-const getSound = ({ portId, buildingId }: Props) => {
+  As Sound is never unmounted, calling pause() as described in
+  https://developer.mozilla.org/en-US/docs/Web/API/HTMLAudioElement/Audio#memory_usage_and_management
+  won’t be necessary.
+ */
+
+const getAudioElement = () => {
+  const audio = new Audio();
+  audio.loop = true;
+  audio.autoplay = true;
+  return audio;
+};
+
+const getTrack = ({ portId, buildingId }: Props) => {
   if (portId === null) {
     return themeOfJoao;
   }
@@ -66,62 +77,65 @@ const getSound = ({ portId, buildingId }: Props) => {
   return mastInTheMist;
 };
 
-export default function Sound({ portId, buildingId }: Props) {
-  const audioRef = useRef(new Audio());
+interface Props {
+  portId: string | null;
+  buildingId: string | null;
+}
 
+export default function Sound({ portId, buildingId }: Props) {
+  const audioRef = useRef(getAudioElement());
+
+  const [hasPlayed, setHasPlayed] = useState(false);
   const [soundOn, setSoundOn] = useState(true);
-  const [currentSound, setCurrentSound] = useState<string>();
+
+  const triggerAutoplay = () => {
+    audioRef.current.play();
+    setHasPlayed(true);
+  };
 
   useEffect(() => {
-    const sound = getSound({ portId, buildingId });
-    setCurrentSound(sound);
+    const track = getTrack({
+      portId,
+      buildingId,
+    });
+
+    // without this check, the track will play from the beginning
+    if (audioRef.current.src !== track) {
+      audioRef.current.src = track;
+    }
   }, [portId, buildingId]);
 
   useEffect(() => {
-    if (!audioRef.current || !currentSound) {
-      return;
+    if (hasPlayed) {
+      return () => {};
     }
 
-    audioRef.current.muted = !soundOn;
+    window.addEventListener('click', triggerAutoplay);
+    window.addEventListener('keydown', triggerAutoplay);
 
-    audioRef.current.loop = true;
-    audioRef.current.src = currentSound;
-    audioRef.current.play();
-  }, [soundOn, currentSound]);
+    return () => {
+      window.removeEventListener('click', triggerAutoplay);
+      window.removeEventListener('keydown', triggerAutoplay);
+    };
+  }, [hasPlayed]);
+
+  useEffect(() => {
+    audioRef.current.muted = !soundOn;
+  }, [soundOn]);
 
   return (
     <div
       className="select-none cursor-pointer p-2 inline-block align-bottom"
       onClick={() => setSoundOn(!soundOn)}
     >
-      {soundOn && (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-8 w-8 text-lime-400"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fillRule="evenodd"
-            d="M9.383 3.076A1 1 0 0110 4v12a1 1 0 01-1.707.707L4.586 13H2a1 1 0 01-1-1V8a1 1 0 011-1h2.586l3.707-3.707a1 1 0 011.09-.217zM14.657 2.929a1 1 0 011.414 0A9.972 9.972 0 0119 10a9.972 9.972 0 01-2.929 7.071 1 1 0 01-1.414-1.414A7.971 7.971 0 0017 10c0-2.21-.894-4.208-2.343-5.657a1 1 0 010-1.414zm-2.829 2.828a1 1 0 011.415 0A5.983 5.983 0 0115 10a5.984 5.984 0 01-1.757 4.243 1 1 0 01-1.415-1.415A3.984 3.984 0 0013 10a3.983 3.983 0 00-1.172-2.828 1 1 0 010-1.415z"
-            clipRule="evenodd"
-          />
-        </svg>
-      )}
-      {!soundOn && (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-8 w-8 text-red-600"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fillRule="evenodd"
-            d="M9.383 3.076A1 1 0 0110 4v12a1 1 0 01-1.707.707L4.586 13H2a1 1 0 01-1-1V8a1 1 0 011-1h2.586l3.707-3.707a1 1 0 011.09-.217zM12.293 7.293a1 1 0 011.414 0L15 8.586l1.293-1.293a1 1 0 111.414 1.414L16.414 10l1.293 1.293a1 1 0 01-1.414 1.414L15 11.414l-1.293 1.293a1 1 0 01-1.414-1.414L13.586 10l-1.293-1.293a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          />
-        </svg>
-      )}
+      {soundOn && <VolumeUpIcon className="h-8 w-8 text-lime-400" />}
+      {!soundOn && <VolumeOffIcon className="h-8 w-8 text-red-600" />}
     </div>
   );
 }
+
+/*
+ TODO
+  Clicks on the sound button will trigger quest dialog as well.
+  Solve it by attaching quest dialog clicks to Interface rather than window.
+ */
