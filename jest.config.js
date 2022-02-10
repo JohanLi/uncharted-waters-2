8f34cdf75de59d3456951bd5212b08f72d18c95a
Johan Li <hi@johan.li>
2022-02-10 21:12:41 +0100

    chore: replace ts-jest with @swc/jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  preset: 'ts-jest',
+  // ts-jest is much slower and not needed as we run tsc as part of build
+  // @swc/jest is faster than Babel
+  transform: {
+    '\\.tsx?$': '@swc/jest',
+  },
   moduleNameMapper: {
     '\\.(png|bin|ogg|mp3)$': 'identity-obj-proxy',
   },
