91f4d436561bb9881926775db5cde7f4f7e0cce3
Johan Li <hi@johan.li>
2022-02-11 23:37:08 +0100

    stateDev

--- src/state/stateDev.ts
+++ src/state/stateDev.ts
@@ -1,3 +1,7 @@
-export default {
+import type { State } from './state';
+
+const stateDev: Partial<State> = {
   // properties defined here will overwrite state. Used during development
 };
+
+export default stateDev;
