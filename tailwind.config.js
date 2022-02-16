5b34ee063a854da312b740e5c3e702f12cfaeade
Johan Li <hi@johan.li>
2022-02-17 00:18:45 +0100

    fix: missing tailwind include

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: ['./src/interface/**/*.{ts,tsx}', './src/homepage/index.html'],
+  content: [
+    './src/homepage/index.html',
+    './src/interface/**/*.{ts,tsx}',
+    './src/data/characterData.ts',
+  ],
   theme: {
     fontFamily: {
       sans: ['Inter', ...defaultTheme.fontFamily.sans],
